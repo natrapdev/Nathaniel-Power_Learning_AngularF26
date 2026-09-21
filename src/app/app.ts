ad418df5e0c2fe8f459a84cf287d74ec35bfb71a
Nathaniel Power <natrap2007@gmail.com>
2026-09-21 15:17:19 -0400

    Initialized Airline array airlineList

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Airline } from './shared/models/airline';
 
 @Component({
   imports: [RouterOutlet],
@@ -11,4 +12,12 @@ export class App {
   protected readonly title = signal('Nathaniel-Power-Learning-AngularF26');
   favNumber : number = 6;
   countryName : string = "Canada";
+
+  airlineList: Airline[] = [
+    { id: 1, name: "Air Canada", iataCode: "AC", callsign: "AIR CANADA", fleetSize: 210, countryName: "Canada", countryIso2: "CA" },
+    { id: 2, name: "Delta Airlines", iataCode: "DL", callsign: "DELTA", fleetSize: "1004", countryName: "United States", countryIso2: "US"},
+    { id: 3, name: "Cathay Pacific", iataCode: "CX", callsign: "CATHAY", countryName: "Hong Kong", countryIso2: "HK" },
+    { id: 4, name: "Cebu Pacific", iataCode: "5J", callsign: "CEBU AIR", countryName: "Philippines", countryIso2: "PH" },
+    { id: 6, name: "WestJet", iataCode: "WS", callsign: "WESTJET", countryName: "Canada", countryIso2: "CA" }
+  ]
 }
